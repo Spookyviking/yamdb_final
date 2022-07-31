998dac4fbde2b08b11d871f03b81f8c3ef2185b9
Maksim Ostapenko <95276551+Spookyviking@users.noreply.github.com>
2022-08-01 00:09:20 +0300

    исправления для автотестов

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,9 +7,10 @@ load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+# для прохождения тестов выставлены дефолтные значения
+SECRET_KEY = os.getenv('SECRET_KEY', default='rk7fiss1=c4dses4g@m_*n=iv+m$o5^+nn=!6ezran*nw^vie(')
 
-DEBUG = os.getenv('DEBUG ')
+DEBUG = os.getenv('DEBUG', default='False')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1').split(' ')
 
@@ -64,10 +65,10 @@ DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', default='postgres'),
-        'USER': os.getenv('POSTGRES_USER', default=None),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default=None),
-        'HOST': os.getenv('DB_HOST', default=None),
-        'PORT': os.getenv('DB_PORT', default=None)
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+        'HOST': os.getenv('DB_HOST', default='db'),
+        'PORT': os.getenv('DB_PORT', default='5432')
     }
 }
 
